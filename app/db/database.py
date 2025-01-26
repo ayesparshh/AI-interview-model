81f55e23807ca17185ca6d9afcf37d07d99aec38
Jvmolu <anmol.singhal50@gmail.com>
2025-01-26 17:12:23 +0530

    Dev

--- app/db/database.py
+++ app/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/AI_SERVICE_DB"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/AiServiceDB"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
